3b3415d2556fca225816d18a13b675e232f7ce16
pranjal singh sengar <67635283+pranjalsinghsengar@users.noreply.github.com>
2023-09-23 15:54:26 +0530

    chens

--- view/index.js
+++ view/index.js
@@ -16,6 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function sendMail() {
+  backData();
   var params = {
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
@@ -27,6 +28,36 @@ function sendMail() {
 
   const serviceID = "service_5mrczlr";
   const templateID = "template_xt82oad";
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.querySelector(".paymentpage").style.display = "none";
+
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("trasectionId").value = "";
+      document.querySelector("#imageInput").value = "";
+      imgBase64 = "";
+
+      console.log(res);
+      alert("Your message sent successfully!!");
+    })
+    .catch((err) => console.log(err));
+}
+
+function backData() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    trasectionId: document.getElementById("trasectionId").value,
+    imgsrc: imgBase64,
+  };
+
+  console.log(params);
+
+  const serviceID = "service_5mrczlr";
+  const templateID = "template_gpg2nel";
 
   emailjs
     .send(serviceID, templateID, params)
